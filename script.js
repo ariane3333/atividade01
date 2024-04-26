1fdf5f7b844bdddc80cb814e1efe59256db4363d
ariane3333 <sabrinaalmeida966@gmail.com>
2024-04-26 09:59:46 -0500

    atividade

---
+++ script.js
@@ -0,0 +1,38 @@
+const dom =1;
+const seg =2;
+const ter =3;
+const qua =4;
+const qui =5;
+const sex =6;
+const sab =7;
+
+let dia = 2;
+
+if(dia ==1){
+document.write ("domingo dia 'de sair' ");
+
+}
+else if (dia ==2){
+     document.write("segunda estudar");
+
+}
+else if (dia ==3){
+    document.write ("terça dia de estudar");
+
+}
+else if (dia ==4){
+    document.write ("quarta dia de ir da uma volta");
+
+}
+else if (dia ==5){
+    document.write ("quinta estudar");
+
+}
+else if (dia ==6){
+      document.write ("sexta se preparar para o fim de semana");
+
+}
+else if (dia ==7){
+      document.write ("sabado dia de procurar uma tabacaria");
+
+}
